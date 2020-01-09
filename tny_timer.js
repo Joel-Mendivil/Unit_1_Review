2c7eb13a9cdfb017f69415063b7414a774a5e979
Joel Mendivil <jmendivil171@west-mec.org>
2020-01-09 15:29:19 -0700

    Almost finished with review

---
+++ tny_timer.js
@@ -0,0 +1,42 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Review Assignment
+
+   Event Timer
+   Author: Joel Mendivil
+   Date: 1/9/20 
+
+*/
+
+showClock();
+setInterval("showClock()", 1000);
+
+function showClock(){
+    var thisDay = new Date("May 19, 2018 9:31:27");
+    var localDate = thisDay.toLocaleDateString();
+    var localTime = thisDay.toLocaleTimeString();
+
+    document.getElementById("currentTime").innerHTML = localDate + "<br />" + localTime;
+
+    var j4Date = nextJuly4("thisDay");
+    j4Date.setHours("21");
+
+    var hrs = (days - Math.floor(days))*24;
+    var mins = (hrs - Math.floor(hrs))*60;
+    var secs = (mins - Math.floor(mins))*60;
+
+    document.getElementById("dLeft").textContent = Math.floor(days);
+    document.getElementById("hLeft").textContent = Math.floor(hrs);
+    document.getElementById("mLeft").textContent = Math.floor(mins);
+    document.getElementById("sLeft").textContent = Math.floor(secs);
+}
+
+function nextJuly4(currentDate) {
+   var cYear = currentDate.getFullYear();
+   var jDate = new Date("July 4, 2018");
+   jDate.setFullYear(cYear);
+   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
+   return jDate;
+}
